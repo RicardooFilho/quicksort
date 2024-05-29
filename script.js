b40adb6e609935d76e250bff131bf352a3f651dd
João Antonio Garcia Rolo <joaorolo@usacucar.com.br>
2024-05-29 13:50:40 -0300

    :sparkles: feat: Função Embaralhar Adicionada

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 import pokemons from "./pokemons.js";
 
-function loadPokemons () {
-
+function loadPokemons(pokemonList) {
     const ul = document.querySelector('ul');
+    ul.innerHTML = '';  // Limpa a lista antes de adicionar os pokémons
 
-    pokemons.forEach(pokemon => {
-
+    pokemonList.forEach(pokemon => {
         ul.innerHTML += `
           <li>
           <img src="${pokemon.img}">
           <span class="header">${pokemon.nome}</span>
           <span class="footer">PS: ${pokemon.ps}</span>
           </li>        
-        `
+        `;
     });
 }
 
-loadPokemons();
+document.getElementById('embaralhar').addEventListener('click', () => {
+    const listaEmbaralhada = _.shuffle(pokemons);
+    loadPokemons(listaEmbaralhada);
+});
+
+loadPokemons(pokemons);
