a80e532247ab2e51897fc3c7551456bd39e4174c
RicardooFilho <ricardo.tagami17@gmail.com>
2024-06-01 21:22:07 -0300

    Implementa ordenação por quick sort

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function loadPokemons(pokemonList) {
 
     const ul = document.querySelector('ul');
 
-    clearUl(ul)
+    clearUl(ul);
 
     pokemonList.forEach(pokemon => {
         ul.innerHTML += `
@@ -22,16 +22,70 @@ function loadPokemons(pokemonList) {
 
 function shufflePokemons() { 
 
-   const listaEmbaralhada = pokemons.sort(() => Math.random() - 0.5)
+   const listaEmbaralhada = pokemons.sort(() => Math.random() - 0.5);
 
-    loadPokemons(listaEmbaralhada)
+    loadPokemons(listaEmbaralhada);
 }
 
-window.shufflePokemons = shufflePokemons
 
 function clearUl(ul) {
+
        ul.innerHTML = '';
 }
 
-loadPokemons(pokemons)
+function quickSortOrdenation(array, startPosition, finalPosition) {
+
+    if (array.length <= 1) {
+
+        return array;
+    }
+
+    let pivot = array[startPosition].ps;
+    let left = startPosition;
+    let right = finalPosition;
+
+    while (left <= right) {
+
+        while (array[left].ps < pivot) {
+
+            left++;
+        }
+
+        while (array[right].ps > pivot) {
+
+            right--;
+        }
+
+        if (left <= right) {
+
+            [array[left], array[right]] = [array[right], array[left]];
+
+            left++;
+            right--;
+        }
+    }
+
+    if (startPosition < right) {
+
+        quickSortOrdenation(array, startPosition, right);
+    }
+
+    if (finalPosition > left) {
+
+        quickSortOrdenation(array, left, finalPosition);
+    }
+
+    return array;
+}
+
+function sortPokemons() {
+
+    const sortedPokemons = quickSortOrdenation(pokemons, 0, pokemons.length - 1);
+
+    loadPokemons(sortedPokemons);
+}
+
+window.shufflePokemons = shufflePokemons;
+window.sortPokemons = sortPokemons;
+loadPokemons(pokemons);
 
